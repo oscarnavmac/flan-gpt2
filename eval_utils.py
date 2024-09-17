b129427d9d5da3611b5b42495206cea847a86226
oscarnavmac <rock.navarrete@hotmail.com>
2024-09-16 21:59:45 -0500

    Added common_gen and xsum tasks to evaluate

--- eval_utils.py
+++ eval_utils.py
@@ -3,8 +3,9 @@ from datasets import load_dataset
 from data_utils import format_example
 from templates import PATTERNS
 from tqdm import tqdm
+import re
 
-def generate(model, tokenizer, device, input_list, tagging):
+def generate(model, tokenizer, device, input_list):
     outputs = []
     for input in tqdm(input_list, desc="Evaluating model"):
         inputs = tokenizer(input, return_tensors='pt').to(device)
@@ -17,19 +18,19 @@ def generate(model, tokenizer, device, input_list, tagging):
                 do_sample=True
             )[0],
             skip_special_tokens=True
-        )[input_length:].strip()#.partition(' ')[0]
+        )[input_length:].strip()
         outputs.append(output)
 
     return outputs
 
 def get_label(generated, tagging):
     word = generated.partition(' ')[0] #.partition(' ')[0] for picking only the first word
+    cleaned_word = re.sub(r'[^\w\s]', '', word) # Dont discard generations such as entailment:, True?, etc...
     try:
-        label = tagging(word)
+        label = tagging(cleaned_word)
     except:
         label = -1
-    #print(word)
-
+    #print(cleaned_word)
     return label
 
 def str2bool(str):
@@ -42,6 +43,12 @@ def str2bool(str):
 def accuracy(references, predictions):
     acc_metric = evaluate.load("accuracy")
     return acc_metric.compute(references=references, predictions=predictions)
+
+def rouge(references, predictions):
+    rouge_metric = evaluate.load('rouge')
+    return rouge_metric.compute(predictions=predictions,
+                  references=references,
+                  use_aggregator=True)
 
 class Evaluation:
     def __init__(self, model, tokenizer, device):
@@ -57,18 +64,35 @@ class Evaluation:
         references = dataset["label"]
         index = 0 # We will be using the first template for any task
         input_list = [format_example(ex, PATTERNS["anli"], index)["prompt"] for ex in dataset]
-        predictions = generate_labels(self.model, self.tokenizer, self.device, input_list[:2], str2int)
-        result = accuracy(references[:2], predictions)["accuracy"]
-        print(f"Total accuracy is {result}")
+        predictions = generate(self.model, self.tokenizer, self.device, input_list[:10])
+        predictions = [get_label(pred, str2int) for pred in predictions]
+        result = accuracy(references[:10], predictions)["accuracy"]
+        return result
 
     def bool_q(self):
         dataset = load_dataset('google/boolq', split='validation')
         references = [int(ans) for ans in dataset["answer"]]
         index = 0 # We will be using the first template for any task
         input_list = [format_example(ex, PATTERNS["bool_q"], index)["prompt"] for ex in dataset]
-        predictions = generate_labels(self.model, self.tokenizer, self.device, input_list[:2], str2bool)
-        result = accuracy(references[:2], predictions)["accuracy"]
-        print(f"Total accuracy is {result}")
+        predictions = generate(self.model, self.tokenizer, self.device, input_list[:5])
+        predictions = [get_label(pred, str2bool) for pred in predictions]
+        result = accuracy(references[:5], predictions)["accuracy"]
+        return result
 
-    def common_gen():
-        pass
+    def common_gen(self):
+        dataset = load_dataset('allenai/common_gen', split='validation')
+        references = dataset["target"]
+        index = 0 # We will be using the first template for any task
+        input_list = [format_example(ex, PATTERNS["common_gen"], index)["prompt"] for ex in dataset]
+        predictions = generate(self.model, self.tokenizer, self.device, input_list[:2])
+        result = rouge(references[:2], predictions)["rouge1"]
+        return result
+
+    def xsum(self):
+        dataset = load_dataset('EdinburghNLP/xsum', split='test')
+        references = dataset["summary"]
+        index = 0 # We will be using the first template for any task
+        input_list = [format_example(ex, PATTERNS["xsum"], index)["prompt"] for ex in dataset]
+        predictions = generate(self.model, self.tokenizer, self.device, input_list[:2])
+        result = rouge(references[:2], predictions)["rougeLsum"]
+        return result
--- test.py
+++ test.py
@@ -10,5 +10,12 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint)
 
 eval = Evaluation(model, tokenizer, device)
 
-eval.anli()
-eval.bool_q()
+res_anli = eval.anli()
+res_boolq = eval.bool_q()
+res_commongen = eval.common_gen()
+res_xsum = eval.xsum()
+
+print(f"Total accuracy on ANLI is {res_anli}")
+print(f"Total accuracy on BoolQ is {res_boolq}")
+print(f"Rouge-1 score on Common Gen is {res_commongen}")
+print(f"Rouge-LSum score on XSum is {res_xsum}")
