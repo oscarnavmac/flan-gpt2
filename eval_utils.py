8e2dd1bf95d86612b36914dbde778ef27681d20c
oscarnavmac <rock.navarrete@hotmail.com>
2024-09-16 21:01:49 -0500

    Added anli and common_gen

--- eval_utils.py
+++ eval_utils.py
@@ -1,9 +1,13 @@
 import evaluate
+from datasets import load_dataset
+from data_utils import format_example
+from templates import PATTERNS
+from tqdm import tqdm
 
-def generate_labels(model, tokenizer, input_list, tagging):
-    labels = []
-    for input in input_list:
-        inputs = tokenizer(input, return_tensors='pt').to(model.get_device())
+def generate(model, tokenizer, device, input_list, tagging):
+    outputs = []
+    for input in tqdm(input_list, desc="Evaluating model"):
+        inputs = tokenizer(input, return_tensors='pt').to(device)
         input_length = len(tokenizer.decode(inputs["input_ids"][0]))
         output = tokenizer.decode(
             model.generate(
@@ -13,13 +17,20 @@ def generate_labels(model, tokenizer, input_list, tagging):
                 do_sample=True
             )[0],
             skip_special_tokens=True
-        )[input_length:].strip().partition(' ')[0] #.partition(' ')[0] for picking only the first word
-        try:
-            labels.append(tagging(output))
-        except:
-            labels.append(-1)
+        )[input_length:].strip()#.partition(' ')[0]
+        outputs.append(output)
 
-    return labels
+    return outputs
+
+def get_label(generated, tagging):
+    word = generated.partition(' ')[0] #.partition(' ')[0] for picking only the first word
+    try:
+        label = tagging(word)
+    except:
+        label = -1
+    #print(word)
+
+    return label
 
 def str2bool(str):
     if str.lower() == "true":
@@ -29,4 +40,35 @@ def str2bool(str):
     else: raise Exception("Not a Boolean")
 
 def accuracy(references, predictions):
-    return evaluate.load("accuracy")
+    acc_metric = evaluate.load("accuracy")
+    return acc_metric.compute(references=references, predictions=predictions)
+
+class Evaluation:
+    def __init__(self, model, tokenizer, device):
+        self.model = model
+        self.tokenizer = tokenizer
+        self.device = device
+
+    def anli(self):
+        dataset = load_dataset("facebook/anli", split="test_r1")
+        int2str = dataset.features['label'].int2str
+        str2int = dataset.features['label'].str2int
+        dataset = dataset.map(lambda example: {"answer": int2str(example["label"])})
+        references = dataset["label"]
+        index = 0 # We will be using the first template for any task
+        input_list = [format_example(ex, PATTERNS["anli"], index)["prompt"] for ex in dataset]
+        predictions = generate_labels(self.model, self.tokenizer, self.device, input_list[:2], str2int)
+        result = accuracy(references[:2], predictions)["accuracy"]
+        print(f"Total accuracy is {result}")
+
+    def bool_q(self):
+        dataset = load_dataset('google/boolq', split='validation')
+        references = [int(ans) for ans in dataset["answer"]]
+        index = 0 # We will be using the first template for any task
+        input_list = [format_example(ex, PATTERNS["bool_q"], index)["prompt"] for ex in dataset]
+        predictions = generate_labels(self.model, self.tokenizer, self.device, input_list[:2], str2bool)
+        result = accuracy(references[:2], predictions)["accuracy"]
+        print(f"Total accuracy is {result}")
+
+    def common_gen():
+        pass
--- test.py
+++ test.py
@@ -1,4 +1,14 @@
-from datasets import load_dataset
+import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from eval_utils import Evaluation
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+checkpoint = "gpt2-multitask-4"
 
+model = AutoModelForCausalLM.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to(device)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+eval = Evaluation(model, tokenizer, device)
+
+eval.anli()
+eval.bool_q()
