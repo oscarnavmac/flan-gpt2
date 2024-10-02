c6e330eb33416da3419248af448bd5f3e2270059
oscarnavmac <rock.navarrete@hotmail.com>
2024-10-01 21:02:22 -0600

    Minor issue

--- kd_train.py
+++ kd_train.py
@@ -1,21 +1,15 @@
 from data_utils import create_instruct_dataset
-from transformers import AutoModelForCausalLM, AutoModelForSeq2SeqLM, AutoTokenizer
-from transformers import DataCollatorForLanguageModeling
-from distill_utils import DistillationTrainer, DistillationTrainingArguments
+from distill_utils import KLDivTrainer, DistillationArguments
+from model_utils import T5Model
 import torch
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-checkpoint = "openai-community/gpt2-medium"
-teacher_name = "google/flan-t5-large"
-repo_name = "gpt2-multitask-distill"
+checkpoint = "google/flan-t5-small"
+teacher_name = "google/flan-t5-base"
+repo_name = "flan-t5-distill-test"
 
-# Load Teacher model
-teacher = AutoModelForSeq2SeqLM.from_pretrained(teacher_name, torch_dtype=torch.bfloat16).to(device)
-
-# Load model and tokenizer
-model = AutoModelForCausalLM.from_pretrained(checkpoint, torch_dtype=torch.bfloat16).to(device)
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-tokenizer.pad_token = tokenizer.eos_token
+student = T5Model(checkpoint, device)
+teacher = T5Model(teacher_name, device)
 
 # Load instruct dataset (4 tasks)
 datasets_names = ["common_gen", "xsum", "bool_q", "anli"]
@@ -23,39 +17,38 @@ dataset = create_instruct_dataset(datasets_names)
 
 # Tokenize examples and initialize clm data collator
 tokenized_dataset = dataset.map(
-    lambda example:  tokenizer(example["text"], truncation=True),
-    remove_columns=["text"],
+    student.tokenize_function,
+    remove_columns=["prompt", "completion"],
     batched=True
 )
-data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
 # Define training arguments
-training_args = DistillationTrainingArguments(
+training_args = DistillationArguments(
     output_dir=repo_name,
     learning_rate=5e-4,
     per_device_train_batch_size=1,
     gradient_accumulation_steps = 4,
-    num_train_epochs=3,
+    num_train_epochs=1,
     weight_decay=0.01,
-    max_steps=10, #quitar
+    max_steps=100, #quitar
     save_steps=1e6,
     #save_strategy='epoch',
-    #logging_steps=100,
+    logging_steps=20,
     use_cpu=False, #CHECK PLEASE
-    push_to_hub=True,
-    hub_model_id=repo_name, 
+    #push_to_hub=True,
+    #hub_model_id=repo_name, 
     alpha=0.5,
     temperature=2.0
 )
 
 # Initialize API Trainer()
-trainer = DistillationTrainer(
-    model,
+trainer = KLDivTrainer(
+    student.get_model(),
     training_args,
-    teacher_model=teacher,
-    data_collator=data_collator,
+    teacher_model=teacher.get_model(),
+    data_collator=student.get_collator(),
     train_dataset=tokenized_dataset,
-    tokenizer=tokenizer
+    tokenizer=student.get_tokenizer()
 )
 
 # Train!!!
