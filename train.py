1fec45f5fa2d44af8a53256e1df497ce202def6f
oscarnavmac <rock.navarrete@hotmail.com>
2024-10-17 17:05:23 -0500

    added logging

--- train.py
+++ train.py
@@ -4,8 +4,10 @@ from data_utils import create_instruct_dataset
 from model_utils import GPT2Model
 from torch.utils.data import DataLoader
 import torch
+import logging
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 checkpoint = 'openai-community/gpt2'
 
 repo_name = "gpt2-dummy-testing"
@@ -30,12 +32,12 @@ tokenized_dataset = dataset.map(
 tokenized_dataset.set_format("torch")
 
 train_dataloader = DataLoader(
-    tokenized_dataset, shuffle=True, batch_size=2, collate_fn=gpt2_model.get_collator()
+    tokenized_dataset, shuffle=True, batch_size=4, collate_fn=gpt2_model.get_collator()
 )
 
 # Define hyperparameters:
 optimizer = AdamW(model.parameters(), lr=5e-5)
-num_epochs = 1
+num_epochs = 2
 num_training_steps = num_epochs * len(train_dataloader)
 lr_scheduler = get_scheduler(
     "linear",
@@ -43,13 +45,17 @@ lr_scheduler = get_scheduler(
     num_warmup_steps=0,
     num_training_steps=num_training_steps
 )
-push_to_hub = False
+push_to_hub = True
 save_model = True
+logging_steps = 100
+save_steps = 1000
 
 progress_bar = tqdm(range(num_training_steps))
+logging.basicConfig(level=logging.INFO)
 
 # TRAIN!!!!!
 model.train()
+global_step = 0
 for epoch in range(num_epochs):
     for batch in train_dataloader:
         #print({k: (v.shape,v) for k, v in batch.items()})
@@ -63,8 +69,19 @@ for epoch in range(num_epochs):
         lr_scheduler.step()
         optimizer.zero_grad()
         progress_bar.update(1)
-    if save_model:
-        model.save(repo_name)
+        global_step+=1
+
+        if global_step % logging_steps == 0:
+            logging.info(f"Loss: {loss}, step: {global_step}")
+
+        if save_model and global_step % save_steps == 0:
+            logging.info("saving model...")
+            model.save_pretrained(repo_name)
+            gpt2_model.get_tokenizer().save_pretrained(repo_name)
+
+if save_model:
+    model.save_pretrained(repo_name)
+    gpt2_model.get_tokenizer().save_pretrained(repo_name)
 
 # Push the model to the repo
 if push_to_hub:
