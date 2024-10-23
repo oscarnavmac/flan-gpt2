b0097b9020bf7296a7be8be831f1063067123698
oscarnavmac <rock.navarrete@hotmail.com>
2024-10-23 13:58:06 -0600

    added tasks

---
+++ tasks.py
@@ -0,0 +1,32 @@
+import datasets
+
+# TODO: implement every dataset processor
+
+# ============================== anli ==========================================
+repo_anli = "facebook/anli"
+
+# ============================== squad_v1 ======================================
+repo_squad_v1 = "rajpurkar/squad"
+
+# ============================== common_gen ====================================
+repo_common_gen = "allenai/common_gen"
+
+# ============================== cosmos_qa =====================================
+repo_cosmos_qa = "allenai/cosmos_qa"
+
+# ================================= CoQA =======================================
+repo_coqa = "stanfordnlp/coqa"
+
+# ================================ human_eval ================================== (NOT ON FLAN: consider manual tamplates)
+repo_human_eval = "openai/openai_humaneval"
+
+# =================================== copa =====================================
+
+# ============================== xsum ==========================================
+repo_xsum = "EdinburghNLP/xsum"
+
+# =============================== glue_mrpc ====================================
+repo_glue_mrpc = "SetFit/mrpc"
+
+# ================================ eng-spa ===================================== (NOT ON FLAN: consider manual tamplates)
+repo_eng_spa = "OscarNav/spa-eng"
